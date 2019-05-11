c0fe7a87c24becc98c957bae769f490c44032bab
Patrick Ngabonziza <ngabonzizap7@.com>
2019-05-11 02:13:08 +0200

    [ft-get-all-users-165950751] get all users

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,13 +1,14 @@
 import express from 'express';
-import userController from '../controllers/user/userController';
+import users from '../controllers/user/allUsers';
+import oneUser from '../controllers/user/oneUser';
 import login from '../controllers/user/signIn';
 import signup from '../controllers/user/signup';
 
 
 const router = express.Router();
 
-router.get('/api/v1/users', userController.getUsers);
-router.get('/api/v1/users/:id', userController.getUser);
+router.get('/api/v1/users', users.getUsers);
+router.get('/api/v1/users/:id', oneUser.getUser);
 router.post('/api/v1/auth/signup', signup.userSignup);
 router.post('/api/v1/auth/signin', login.signin);
 
