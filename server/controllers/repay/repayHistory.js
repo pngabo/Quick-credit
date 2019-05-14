7934db86d27e5c1d294a402395cb5dfad4d9f9ec
Patrick Ngabonziza <ngabonzizap7@.com>
2019-05-14 15:07:15 +0200

    [bug-feedback-LFA]

---
+++ server/controllers/repay/repayHistory.js
@@ -0,0 +1,2 @@
+import repayments from '../../models/repaymentDb';
+
--- server/routes/loan.js
+++ server/routes/loan.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import oneloan from '../controllers/loan/oneLoan';
-import allLoans from '../controllers/loan/allLoans';
 import applyLoan from '../controllers/loan/applyLoan';
 import approveLoan from '../controllers/loan/approveLoan';
+import loanData from '../controllers/loan/getLoans';
 
 const router = express.Router();
-
-router.get('/api/v1/loans', allLoans.getLoans);
 router.get('/api/v1/loans/:id', oneloan.getLoan);
 router.post('/api/v1/loans', applyLoan.loanApply);
 router.patch('/api/v1/loans/:id',approveLoan.approve);
+router.get('/api/v1/loans', loanData.getCurrentLoans);
+
 export default router;
