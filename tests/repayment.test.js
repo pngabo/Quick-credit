4fe8fa51ccbfccd264ed3b2e0843cefb64c501ef
Patrick Ngabonziza <ngabonzizap7@.com>
2019-05-22 23:03:38 +0200

    add loan model

--- tests/repayment.test.js
+++ tests/repayment.test.js
@@ -6,17 +6,17 @@ chai.should();
 chai.use(chaiHttp);
 
 describe('LOAN REPAYMENT', () => {
-    it('User should be able to pay a loan', (done) => {
-        const repayments = {
-          paidAmount: '10000',
-        };
-        chai.request(app)
-          .post('/api/v1/loans/1/repayments')
-          .send(repayments)
-          .end((err, res) => {
-            res.should.have.property('status').eql(201);
-            res.body.should.be.a('object');
-            done();
-          });
+  it('User should be able to pay a loan', (done) => {
+    const repayments = {
+      paidamount: 10000,
+    };
+    chai.request(app)
+      .post('/api/v1/loans/1/repayments')
+      .send(repayments)
+      .end((err, res) => {
+        res.should.have.property('status').eql(201);
+        res.body.should.be.a('object');
+        done();
       });
+  });
 });
