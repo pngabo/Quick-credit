9455e687edf8be4ad1e6109ede720675ddcbe5ac
Patrick Ngabonziza <ngabonzizap7@.com>
2019-05-24 12:20:33 +0200

    test

--- tests/user.test.js
+++ tests/user.test.js
@@ -5,6 +5,19 @@ import chaiHttp from 'chai-http';
 chai.should();
 chai.use(chaiHttp);
 
+let token = '';
+describe('Homepage', () => {
+  describe('HOMEPAGE', () => {
+    it('it should open the homepage', (done) => {
+      chai.request(app)
+        .get('/')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          done();
+        });
+    });
+  });
 describe('USER TEST', () => {
   it('Should create a new user', (done) => {
     const user = {
@@ -14,7 +27,7 @@ describe('USER TEST', () => {
       address: 'Kigali, Kicukiro',
       phonenumber: "078409345324",
       occupation: "SoftwareD",
-      email: 'ngabop7@gmail.com',
+      email: 'ngabop77@gmail.com',
       password: '12345',
     };
 
@@ -29,11 +42,30 @@ describe('USER TEST', () => {
         done();
       });
   });
- 
+  it('it should not register a new user with empty field', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+          firstname: ' ',
+          lastname: 'Ngabonziza',
+          gender: 'Male',
+          address: 'Kigali, Kicukiro',
+          phonenumber: "078409345324",
+          occupation: "SoftwareD",
+          email: 'ngabop7@gmail.com',
+          password: '12345',
+      })
+      .end((err, res) => {
+        res.should.have.status(400);
+        res.body.should.be.a('object');
+        done();
+      });
+  });
   it('It should return the list of all users', (done) => {
     chai
       .request(app)
       .get('/api/v1/users')
+      .set('Authorization', token)
       .end((error, res) => {
         if (error) done(error);
         res.should.have.status(200);
@@ -45,6 +77,7 @@ describe('USER TEST', () => {
     chai
       .request(app)
       .get('/api/v1/users/1')
+      .set('Authorization', token)
       .end((error, res) => {
         if (error) done(error);
         res.should.have.status(200);
@@ -64,7 +97,10 @@ describe('USER TEST', () => {
         res.body.data.should.have.property('token');
         res.body.data.should.have.property('status');
         res.body.should.have.property('isadmin');
+        token = res.body.data[0].token;
       });
     done();
   });
+
+});
 });
